5f6860a8da18c0298d1c16e6bce83f07bcb04f42
Abraham Onyeka Kanime <111146026+AbramKanime@users.noreply.github.com>
2023-09-19 16:11:36 +0100

    Created and added new files

---
+++ index.js
@@ -0,0 +1,95 @@
+import {nanoid} from "nanoid"
+
+const taskList = document.getElementById('taskList')
+const filterEl = document.getElementById("filter")
+const localStorageTodoList = JSON.parse(localStorage.getItem("todoListArray")) || []
+displayTasks()
+
+document.getElementById("add-btn").addEventListener("click", () => {
+    const taskInput = document.getElementById('taskInput')
+    const taskText = taskInput.value.trim()
+    
+    if (taskText === '') {
+        return; // Don't add empty tasks
+    }
+    
+    createTaskObject(taskText)
+    displayTasks()
+    
+    taskInput.value = '' // Clear the input field
+})
+
+document.addEventListener("click", e => { 
+    // These are set up to handle deletion and checking off of tasks
+    if (e.target.dataset.remove) {
+        const id = e.target.dataset.remove
+        for (let item of localStorageTodoList) {
+            if (item.id === id) {
+                const index = localStorageTodoList.indexOf(item)
+                localStorageTodoList.splice(index, 1)
+                localStorage.setItem("todoListArray", JSON.stringify(localStorageTodoList))
+                displayTasks()
+            }
+        }
+    }
+    if (e.target.dataset.id) {
+        for (let item of localStorageTodoList) {
+            if (e.target.dataset.id === item.id) {
+                item.isChecked = e.target.checked ? true : false
+                localStorage.setItem("todoListArray", JSON.stringify(localStorageTodoList))
+            }
+        }
+    }
+})
+
+filterEl.addEventListener("change", () => {
+    let status
+    let filteredArray = []
+    
+    if (filterEl.value === "completed") {
+        status = true
+    } else if (filterEl.value === "active") {
+        status = false
+    } else {
+        return displayTasks()
+    }
+    filteredArray = localStorageTodoList.filter((item) => item.isChecked === status)
+    taskList.innerHTML = ''
+    filteredArray.map(item => {
+        const {task, id, isChecked} = item
+        return createTask(task, id, isChecked)
+    }).join('')
+})
+
+function displayTasks() {
+    taskList.innerHTML = ''
+    localStorageTodoList.map(item => {
+        const {task, id, isChecked} = item
+        return createTask(task, id, isChecked)
+    }).join('')
+}
+
+function createTaskObject(taskText) {
+    const TaskObj = {
+        id: nanoid(),
+        task: taskText,
+        isChecked: false
+    }
+    localStorageTodoList.push(TaskObj)
+    localStorage.setItem("todoListArray", JSON.stringify(localStorageTodoList))
+}
+
+function createTask(taskText, id, isChecked) {
+    const checked = isChecked ? "checked" : ''
+    const taskItem = document.createElement('div')
+    taskItem.className = 'task-item'
+    taskItem.innerHTML = `
+        <input class="check-input" ${checked} type="checkbox" data-id="${id}" />
+        <span id=${id}>${taskText}</span>
+        <div>
+            <button data-remove="${id}" class="remove-btn">Remove</button>
+        </div>
+    `
+
+    taskList.appendChild(taskItem)
+}
